2dc9204e0202e6d13a4fdc37ef757deafba23bc6
Nicholas <nicholas.ackley56@gmail.com>
2025-02-13 03:00:04 -0600

    trouble shooting refreshing all pages

--- src/MainPage/Navbar/Navbar.jsx
+++ src/MainPage/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <span><b>nicholasAckley();</b></span>
       </div>
       <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/website">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/blog">Blog</Link></li>
