85c520decb042208c4f462afca653bc0643f654c
Nicholas <nicholas.ackley56@gmail.com>
2025-02-13 19:40:16 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/website/' : '/', // Use '/website/' only in production
+  base: "/website", // Base path for deployment
   server: {
-    historyApiFallback: true, // Enable fallback for React Router in dev
+    historyApiFallback: true, // Enable fallback for React Router
   },
 });
