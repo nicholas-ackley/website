2539ebf94463f30f6f10cf91b215d2da2dc4a1f6
Nicholas <nicholas.ackley56@gmail.com>
2025-04-02 13:32:11 -0500

    added projects.jsx file and updated css, fixed some more media querys

--- src/ProjectsPage/Projects.jsx
+++ src/ProjectsPage/Projects.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
-import './Projects.css'
+import React from "react";
+import "./Projects.css";
+import Greek from "../images/greekpop.png";
+import Website from "../images/website.png";
+
 const Projects = () => {
   return (
-    <>
-    <div className="projects-title_container">
-      <div className="title">
-        Past Project Experience
-      </div>
-    </div>
-    
-    
-    
-    
-    </>
-  )
-}
+    <section className="projects-section">
+      <h2 className="projects-title">Past Project Experience</h2>
+      <p className="projects-subtitle">
+        Explore the <span>projects I've worked on so far</span>
+      </p>
 
-export default Projects
+      <div className="projects-grid">
+        <div className="project-card">
+          <div className="project-thumbnail">
+            <img
+              src={Website}
+              alt="Personal Website"
+              className="project-image"
+            />
+          </div>
+          <h3 className="project-name">Personal Website</h3>
+          <p className="project-description">
+            A portfolio site showcasing my background, resume, and contact info.
+          </p>
+          <p className="project-tech">React, CSS, JavaScript</p>
+        </div>
+
+        <div className="project-card">
+          <div className="project-thumbnail">
+            <img src={Greek} alt="Sahabat Muslim" className="project-image" />
+          </div>
+          <h3 className="project-name"> Greek-Pop</h3>
+          <p className="project-description">
+            An App where merchandise can be resold and reused for college
+            students.
+          </p>
+          <p className="project-tech">React Native, Firebase</p>
+        </div>
+
+        <div className="project-card">
+          <div className="project-thumbnail">
+            <img src={Greek} alt="SISTEKDES" className="project-image" />
+          </div>
+          <h3 className="project-name">Tinder Replica</h3>
+          <p className="project-description">
+            A replica web application of Tinder
+          </p>
+          <p className="project-tech">Firebase, React</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
